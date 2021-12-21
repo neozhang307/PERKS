836dc0f62de1abce94a20c0bc054285476d2a259
Lingqi <zlqi@qq.com>
2021-12-21 11:03:43 +0900

    iteratively run baseline_cm

---
+++ stencil/2dstencil/iteratebaseline_cm.sh
@@ -0,0 +1,112 @@
+
+source ./domain.sh
+source ../temp.sh
+
+
+list_used=(${test_list[@]})
+if [ "${GPU}" = "v100" ] 
+then
+	# list_used=(${v100_list[@]})
+	echo "current GPU is " ${GPU}
+elif [ "${GPU}" = "a100" ]
+then
+	echo "current GPU is " ${GPU}
+else
+	echo "usupport" ${GPU}
+	exit 1
+fi
+
+rununit()
+{
+	for((i=0; i<${TEST}; i++))
+	do
+		waittilltemp60c
+		./${executable} $WIDTH $HEIGHT --iter=${ITER} --bdim=${BDIM} --blkpsm=0 ${FLOATTYPE} ${CHECK} >> ./${FILE}
+		waittilltemp60c
+		./${executable} $WIDTH $HEIGHT --iter=${ITER} --bdim=${BDIM} --blkpsm=2 ${FLOATTYPE} ${CHECK} >> ./${FILE}
+		waittilltemp60c
+		./${executable} $WIDTH $HEIGHT --iter=${ITER} --bdim=${BDIM} --blkpsm=1 ${FLOATTYPE} ${CHECK} >> ./${FILE}
+	done
+}
+runtest()
+{
+	if((y!=x))
+	then
+		echo "now" $x $y
+		echo "now" $y $x
+		let WIDTH=${x}
+		let HEIGHT=${y}
+		rununit
+		let WIDTH=${y}
+		let HEIGHT=${x}
+		rununit
+	
+	else
+		echo "now" $x $x
+		let WIDTH=${x}
+		let HEIGHT=${x}
+		rununit
+	fi
+
+}
+
+prefix=baseline
+
+ITER=3
+FLOATTYPE="--fp32"
+y=${list_used[0]}
+
+type=("fp32" "fp64")
+source ./domain.sh
+
+
+
+FILEPREFIX="runbaseline"
+executable="*baseline_cm.exe"
+
+
+export ITER=3
+export BDIM=256
+export TEST=1
+CHECK="--check"
+for ltype in ${type[@]};
+do
+	variablex=${ltype}_${GPU}_x
+	variabley=${ltype}_${GPU}_y
+
+	FLOATTYPE="--${ltype}"
+
+	x=${!variablex}
+	y=${!variabley}
+	
+	FILE=${FILEPREFIX}_${ltype}_${GPU}_check.log
+	echo ${FILE}
+	rm ${FILE}
+	echo ${y} ${x} ${FLOATTYPE}
+	runtest
+done 
+
+
+export ITER=1000
+export BDIM=256
+export TEST=10
+CHECK=""
+for ltype in ${type[@]};
+do
+	variablex=${ltype}_${GPU}_x
+	variabley=${ltype}_${GPU}_y
+
+	FLOATTYPE="--${ltype}"
+
+	x=${!variablex}
+	y=${!variabley}
+	
+	FILE=${FILEPREFIX}_${ltype}_${GPU}.log
+	echo ${FILE}
+	rm ${FILE}
+	echo ${y} ${x} ${FLOATTYPE}
+	runtest
+done 
+
+
+
