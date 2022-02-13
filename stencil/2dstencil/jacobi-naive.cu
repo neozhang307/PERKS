53dec7c665a845fce53dca18db93a33eff674574
Lingqi <zlqi@qq.com>
2022-02-13 22:27:04 +0900

    Force not to unroll, or the nvcc version would be too fast

--- stencil/2dstencil/jacobi-naive.cu
+++ stencil/2dstencil/jacobi-naive.cu
@@ -19,66 +19,88 @@ kernel2d_restrict_box
 
 #ifndef BOX
   int c = l_x + l_y * width_x;
-  int w[halo];
-  int e[halo];
-  int n[halo];
-  int s[halo];
-  _Pragma("unroll") 
-  for(int hl=0; hl<halo; hl++)
-  {
-    w[hl] = max(0,l_x-1-hl)+l_y * width_x;
-    e[hl] = min(width_x-1,l_x+1+hl)+l_y * width_x;
-    s[hl] = l_x+max(0,l_y-1-hl) * width_x;;
-    n[hl] = l_x+min(width_y-1,l_y+1+hl) * width_x;
-  }
+  // int w[halo];
+  // int e[halo];
+  // int n[halo];
+  // int s[halo];
+  // // _Pragma("unroll") 
+  // for(int hl=0; hl<halo; hl++)
+  // {
+  //   w[hl] = max(0,l_x-1-hl)+l_y * width_x;
+  //   e[hl] = min(width_x-1,l_x+1+hl)+l_y * width_x;
+  //   s[hl] = l_x+max(0,l_y-1-hl) * width_x;;
+  //   n[hl] = l_x+min(width_y-1,l_y+1+hl) * width_x;
+  // }
   REAL sum=0;
-  _Pragma("unroll") 
+  // _Pragma("unroll") 
+#ifndef NAIVENVCC
+  #pragma unroll 1
+#endif
   for(int hl=0; hl<halo; hl++)
   {
-    sum+=south[hl]*input[s[hl]];
+    // sum+=south[hl]*input[s[hl]];
+    sum+=south[hl]*input[l_x+max(0,l_y-1-hl) * width_x];
   }
-  _Pragma("unroll") 
+  // _Pragma("unroll") 
+#ifndef NAIVENVCC
+  #pragma unroll 1
+#endif
   for(int hl=0; hl<halo; hl++)
   {
-    sum+=west[hl]*input[w[hl]];
+    // sum+=west[hl]*input[w[hl]];
+    sum+=west[hl]*input[max(0,l_x-1-hl)+l_y * width_x];
   }
   sum+=center*input[c];
-  _Pragma("unroll") 
+  // _Pragma("unroll") 
+#ifndef NAIVENVCC
+  #pragma unroll 1
+#endif
   for(int hl=0; hl<halo; hl++)
   {
-    sum+=east[hl]*input[e[hl]];
+    // sum+=east[hl]*input[e[hl]];
+    sum+=east[hl]*input[min(width_x-1,l_x+1+hl)+l_y * width_x];
   }
-  _Pragma("unroll") 
+  // _Pragma("unroll") 
+#ifndef NAIVENVCC
+  #pragma unroll 1
+#endif
   for(int hl=0; hl<halo; hl++)
   {
-    sum+=north[hl]*input[n[hl]];
+    // sum+=north[hl]*input[n[hl]];
+    sum+=north[hl]*input[l_x+min(width_y-1,l_y+1+hl) * width_x];
   }
   output[c]=sum;
   return;
 #else
-  int vertical[HALO*2+1];
-  int horizontal[HALO*2+1];
-  #pragma unroll
-  for(int hl_y=-HALO; hl_y<=HALO; hl_y++)
-  {
-    vertical[hl_y+HALO]=min(max(l_y+hl_y,0),width_y-1)*width_x;
-  }
-  #pragma unroll
-  for(int hl_x=-HALO; hl_x<=HALO; hl_x++)
-  {
-    horizontal[hl_x+HALO]=min(max(l_x+hl_x,0),width_x-1);
-  }
+  // int vertical[HALO*2+1];
+  // int horizontal[HALO*2+1];
+  // #pragma unroll
+  // for(int hl_y=-HALO; hl_y<=HALO; hl_y++)
+  // {
+  //   vertical[hl_y+HALO]=min(max(l_y+hl_y,0),width_y-1)*width_x;
+  // }
+  // #pragma unroll
+  // for(int hl_x=-HALO; hl_x<=HALO; hl_x++)
+  // {
+  //   horizontal[hl_x+HALO]=min(max(l_x+hl_x,0),width_x-1);
+  // }
   REAL sum=0;
-  #pragma unroll
+  // #pragma unroll
+#ifndef NAIVENVCC
+  #pragma unroll 1
+#endif
   for(int hl_y=-HALO; hl_y<=HALO; hl_y++)
   {
-    #pragma unroll 
+#ifndef NAIVENVCC
+  #pragma unroll 1
+#endif
     for(int hl_x=-HALO; hl_x<=HALO; hl_x++)
     {
-      sum+=filter[hl_y+HALO][hl_x+HALO]*input[vertical[hl_y+HALO]  + horizontal[hl_x+HALO]];
+      sum+=filter[hl_y+HALO][hl_x+HALO]*
+      input[min(max(l_y+hl_y,0),width_y-1)*width_x  + min(max(l_x+hl_x,0),width_x-1)];
     }
   }
-  output[vertical[HALO]  + horizontal[HALO]]=sum;
+  output[l_y*width_x  + l_x]=sum;
   return;
 #endif
 }
