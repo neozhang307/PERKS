324dae2b12765c069e00a6f52edc5fd963f0875e
Lingqi <zlqi@qq.com>
2022-02-21 18:16:48 +0900

    Update for situation of double tile python script

--- stencil/2dstencil/ProcessCompilerLog.py
+++ stencil/2dstencil/ProcessCompilerLog.py
@@ -48,10 +48,10 @@ def parseptx(contents, funcname):
 
 import subprocess
 
-def compile(archstring, halostring, regfolderstring, realstring,useSM,asyncSM,box,btype):
-    basicstring="nvcc -std=c++14 --cubin -gencode {0} -Xptxas \"-v\" -DCONFIGURE,HALO={1},TYPE={2},RTILE_Y=8,RTILE_X=256,REG_FOLDER_Y={3}{4}{5}{6} -DBLOCKTYPE={7} ./jacobi-general.cu"
+def compile(archstring, halostring, regfolderstring, realstring,useSM,asyncSM,box,btype,isSmall,TILE_Y):
+    basicstring="nvcc -std=c++14 --cubin -gencode {0} -Xptxas \"-v\" -DCONFIGURE,HALO={1},TYPE={2},RTILE_Y={9},RTILE_X=256,REG_FOLDER_Y={3}{4}{5}{6}{8} -DBLOCKTYPE={7} ./jacobi-general.cu"
 
-    generated_string=basicstring.format(archstring,halostring,realstring,regfolderstring,useSM,asyncSM,box,btype)
+    generated_string=basicstring.format(archstring,halostring,realstring,regfolderstring,useSM,asyncSM,box,btype,isSmall,TILE_Y)
     # print(generated_string)
 
     sub = subprocess.Popen(generated_string, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -61,19 +61,19 @@ def compile(archstring, halostring, regfolderstring, realstring,useSM,asyncSM,bo
 import sys
 
 
-def singletask(filename,halostring,realstring,useSM,box,archstring,asyncSM,btype):
+def singletask(filename,halostring,realstring,useSM,box,archstring,asyncSM,btype,isSmall,TILE_Y):
     regfolder=0
     file = open(filename, 'a')
     original_out=sys.stdout
     while (True):
     # for regfolder in range(3):
-        subprocess_return = compile(archstring,halostring,regfolder,realstring,useSM,asyncSM,box,btype)
+        subprocess_return = compile(archstring,halostring,regfolder,realstring,useSM,asyncSM,box,btype,isSmall,TILE_Y)
         print(subprocess_return)
         returnlist =parseptx(subprocess_return.splitlines(), "general")
         spillnum=int(returnlist[6])+int(returnlist[7])
         # file.write("kernel_general",halostring,returnlist[0],returnlist[1],returnlist[2],returnlist[3],returnlist[4],returnlist[5],returnlist[6],returnlist[7])
         sys.stdout = file 
-        print("kernel_general",halostring,returnlist[0],returnlist[1],returnlist[2],returnlist[3],returnlist[4],returnlist[5],returnlist[6],returnlist[7])
+        print("kernel_general",TILE_Y,halostring,returnlist[0],returnlist[1],returnlist[2],returnlist[3],returnlist[4],returnlist[5],returnlist[6],returnlist[7])
         sys.stdout = original_out
 
         regfolder+=1
@@ -81,30 +81,31 @@ def singletask(filename,halostring,realstring,useSM,box,archstring,asyncSM,btype
         if spillnum>=128:
             break
 
-def HuseSM(filename,halostring,realstring,box,archstring,asyncSM,btype):
-        singletask(filename,halostring,realstring,"",box,archstring,asyncSM,btype)
-        singletask(filename,halostring,realstring,",USESM",box,archstring,asyncSM,btype)
+def HuseSM(filename,halostring,realstring,box,archstring,asyncSM,btype,isSmall,TILE_Y):
+        singletask(filename,halostring,realstring,"",box,archstring,asyncSM,btype,isSmall,TILE_Y)
+        singletask(filename,halostring,realstring,",USESM",box,archstring,asyncSM,btype,isSmall,TILE_Y)
 
-def HuseSMreal(filename,halostring,box,archstring,asyncSM,btype):
-    HuseSM(filename,halostring,"float",box,archstring,asyncSM,btype)
-    HuseSM(filename,halostring,"double",box,archstring,asyncSM,btype)
+def HuseSMreal(filename,halostring,box,archstring,asyncSM,btype,isSmall):
+    HuseSM(filename,halostring,"float",box,archstring,asyncSM,btype,isSmall,8)
+    HuseSM(filename,halostring,"float",box,archstring,asyncSM,btype,isSmall,16)
+    HuseSM(filename,halostring,"double",box,archstring,asyncSM,btype,isSmall,8)
 
-def Hrange(filename,halostart,haloend,box,archstring,asyncSM,btype):
+def Hrange(filename,halostart,haloend,box,archstring,asyncSM,btype,isSmall):
     for i in range(halostart,haloend+1):
-        HuseSMreal(filename,i,box,archstring,asyncSM,btype)
+        HuseSMreal(filename,i,box,archstring,asyncSM,btype,isSmall)
 
-def blockTYPE(filename,halostart,haloend,box,archstring,asyncSM):
-    Hrange(filename,halostart,haloend,box,archstring,asyncSM,1)
-    Hrange(filename,halostart,haloend,box,archstring,asyncSM,2)
+# def blockTYPE(filename,halostart,haloend,box,archstring,asyncSM):
+#     Hrange(filename,halostart,haloend,box,archstring,asyncSM,1)
+#     Hrange(filename,halostart,haloend,box,archstring,asyncSM,2)
 
-def HuseSMrealbox(halostring,archstring,asyncSM):
-    HuseSMreal(halostring,"",archstring,asyncSM)
-    HuseSMreal(halostring,",BOX",archstring,asyncSM)
+# def HuseSMrealbox(halostring,archstring,asyncSM):
+#     HuseSMreal(halostring,"",archstring,asyncSM)
+#     HuseSMreal(halostring,",BOX",archstring,asyncSM)
 
-def HuseSMrealboxArch(halostring):
-    HuseSMrealbox(halostring,"arch=compute_80,code=sm_80","")
-    HuseSMrealbox(halostring,"arch=compute_80,code=sm_80",",ASYNCSM")
-    HuseSMrealbox(halostring,"arch=compute_70,code=sm_70","")
+# def HuseSMrealboxArch(halostring):
+#     HuseSMrealbox(halostring,"arch=compute_80,code=sm_80","")
+#     # HuseSMrealbox(halostring,"arch=compute_80,code=sm_80",",ASYNCSM")
+#     HuseSMrealbox(halostring,"arch=compute_70,code=sm_70","")
 
 
 archstring="arch=compute_80,code=sm_80"
@@ -121,20 +122,31 @@ halostring=1
 # print("star")
 
 
+Hrange("star_80_128.log",1,6,"","arch=compute_80,code=sm_80","",2,"")
+Hrange("star_70_128.log",1,6,"","arch=compute_70,code=sm_70","",2,"")
 
-Hrange("star_80_128.log",1,6,"","arch=compute_80,code=sm_80","",2)
-Hrange("star_80_async_128.log",1,6,"","arch=compute_80,code=sm_80",",ASYNCSM",2)
-Hrange("star_70_128.log",1,6,"","arch=compute_70,code=sm_70","",2)
-
-Hrange("box_80_128.log",1,2,",BOX","arch=compute_80,code=sm_80","",2)
-Hrange("box_80_async_128.log",1,2,",BOX","arch=compute_80,code=sm_80",",ASYNCSM",2)
-Hrange("box_70_128.log",1,2,",BOX","arch=compute_70,code=sm_70","",2)
+Hrange("box_80_128.log",1,2,",BOX","arch=compute_80,code=sm_80","",2,"")
+Hrange("box_70_128.log",1,2,",BOX","arch=compute_70,code=sm_70","",2,"")
 
 
-Hrange("star_80_256.log",1,6,"","arch=compute_80,code=sm_80","",1)
-Hrange("star_80_async_256.log",1,6,"","arch=compute_80,code=sm_80",",ASYNCSM",1)
-Hrange("star_70_256.log",1,6,"","arch=compute_70,code=sm_70","",1)
+Hrange("star_80_256.log",1,6,"","arch=compute_80,code=sm_80","",1,"")
+Hrange("star_70_256.log",1,6,"","arch=compute_70,code=sm_70","",1,"")
 
-Hrange("box_80_256.log",1,2,",BOX","arch=compute_80,code=sm_80","",1)
-Hrange("box_80_async_256.log",1,2,",BOX","arch=compute_80,code=sm_80",",ASYNCSM",1)
-Hrange("box_70_256.log",1,2,",BOX","arch=compute_70,code=sm_70","",1)
+Hrange("box_80_256.log",1,2,",BOX","arch=compute_80,code=sm_80","",1,"")
+Hrange("box_70_256.log",1,2,",BOX","arch=compute_70,code=sm_70","",1,"")
+
+
+Hrange("star_80_128_small.log",1,6,"","arch=compute_80,code=sm_80","",2,",SMALL")
+Hrange("star_70_128_small.log",1,6,"","arch=compute_70,code=sm_70","",2,",SMALL")
+
+Hrange("box_80_128_small.log",1,2,",BOX","arch=compute_80,code=sm_80","",2,",SMALL")
+Hrange("box_70_128_small.log",1,2,",BOX","arch=compute_70,code=sm_70","",2,",SMALL")
+
+
+Hrange("star_80_256_small.log",1,6,"","arch=compute_80,code=sm_80","",1,",SMALL")
+Hrange("star_70_256_small.log",1,6,"","arch=compute_70,code=sm_70","",1,",SMALL")
+
+Hrange("box_80_256_small.log",1,2,",BOX","arch=compute_80,code=sm_80","",1,",SMALL")
+Hrange("box_70_256_small.log",1,2,",BOX","arch=compute_70,code=sm_70","",1,",SMALL")
+
+
--- stencil/2dstencil/ProcessRsts2cuh.py
+++ stencil/2dstencil/ProcessRsts2cuh.py
@@ -11,9 +11,9 @@ import pandas as pd
 
 
 def parsedata(shape,arch,register):
-	df_none = pd.read_csv('./{}_{}_{}.log'.format(shape,arch,register), names=['name', 'halo', 'arch', 'type', 'usesm', 'regfd','regs', 'sm', 'spilla', 'spillb'], header=None, delimiter=' ')
+	df_none = pd.read_csv('./{}_{}_{}.log'.format(shape,arch,register), names=['name', 'tile', 'halo', 'arch', 'type', 'usesm', 'regfd','regs', 'sm', 'spilla', 'spillb'], header=None, delimiter=' ')
 	nospill=df_none[(df_none['spilla']==0)&(df_none['spillb']==0)]
-	d_result=nospill.groupby(["halo","arch","type","usesm"])["regfd"].max().reset_index()
+	d_result=nospill.groupby(["halo","arch","type","usesm",'tile'])["regfd"].max().reset_index()
 
 	for row in d_result.iterrows():
 		# print("#if HALO == {}".format(row[1]["halo"]))
@@ -21,13 +21,14 @@ def parsedata(shape,arch,register):
 			# format(shape,register,row[1]["arch"],row[1]["type"],"_usm" if row[1]["usesm"]==1 else "",row[1][4]))
 
 		print("template<>")
-		print("struct regfolder<{},{},{},{},{},{}>".
+		print("struct regfolder<{},{},{},{},{},{},{}>".
 			format(row[1]["halo"], 
 				"true" if shape=="star" else "false",
 				register,
 				int(arch)*10,
 				"true" if row[1]["usesm"]==1 else "false",
-				"float" if row[1]["type"]=="f" else "double"
+				"float" if row[1]["type"]=="f" else "double",
+				row[1]["tile"]
 				))
 		print("{")
 		print("\tstatic int const val = {};".format(row[1][4]))
@@ -44,7 +45,7 @@ import sys
 file = open("perksconfig.cuh", 'w')
 sys.stdout = file
 print("#pragma once")
-print("template<int halo, bool isstar, int registeramount, int arch, bool useSM, class REAL>")
+print("template<int halo, bool isstar, int registeramount, int arch,  bool useSM, class REAL, int tile=8>")
 print("struct regfolder")
 print("{")
 print("\tstatic int const val = 0;")
