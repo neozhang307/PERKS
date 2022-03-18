f9b969ce34e7fdc6357248fc1035e3c89b30fc30
Lingqi <zlqi@qq.com>
2022-03-18 18:24:31 +0900

    update script

--- stencil/2dstencil/iteratedomain.sh
+++ stencil/2dstencil/iteratedomain.sh
@@ -1,8 +1,11 @@
 
 
 test_list=(512 768)
-a100_list=(512 768 1024 1536 2304 3072 4608 6912 9216 13824 18432 27648)
-v100_list=(512 1024 1280 2048 2560 4096 5120 8192 10240 16384 20480)
+# a100_list=(512 768 1024 1536 2304 3072 4608 6912 9216 13824 18432 27648)
+# v100_list=(512 1024 1280 2048 2560 4096 5120 8192 10240 16384 20480)
+a100_list=(512 768 1024 1536 2304 3072 4608 6912 9216 13824 18432)
+v100_list=(512 1024 1280 2048 2560 4096 5120 8192 10240 16384)
+
 
 
 source ../temp.sh
@@ -26,17 +29,17 @@ rununit()
 {
 	for((i=0; i<${TEST}; i++))
 	do
-		if [ "${GPU}" = "a100" ]
-		then
-			waittilltemp60c
-			./*baseline.exe $WIDTH $HEIGHT --iter=${ITER} --bdim=128 ${FLOATTYPE} ${CHECK} --async >> ./${FILE}
-			waittilltemp60c
-			./*baseline.exe $WIDTH $HEIGHT --iter=${ITER} --bdim=256 ${FLOATTYPE} ${CHECK} --async >> ./${FILE}
-		fi
+		# if [ "${GPU}" = "a100" ]
+		# then
+		# 	waittilltemp60c
+		# 	./*baseline.exe $WIDTH $HEIGHT --iter=${ITER} --bdim=128 ${FLOATTYPE} ${CHECK} --async >> ./${FILE}
+		# 	waittilltemp60c
+		# 	./*baseline.exe $WIDTH $HEIGHT --iter=${ITER} --bdim=256 ${FLOATTYPE} ${CHECK} --async >> ./${FILE}
+		# fi
 		waittilltemp60c
-		./*baseline.exe $WIDTH $HEIGHT --iter=${ITER} --bdim=128 ${FLOATTYPE} ${CHECK} >> ./${FILE}
+		./*baseline.exe $WIDTH $HEIGHT --iter=${ITER} --bdim=128 ${FLOATTYPE} ${CHECK} --warmup>> ./${FILE}
 		waittilltemp60c
-		./*baseline.exe $WIDTH $HEIGHT --iter=${ITER} --bdim=256 ${FLOATTYPE} ${CHECK} >> ./${FILE}
+		./*baseline.exe $WIDTH $HEIGHT --iter=${ITER} --bdim=256 ${FLOATTYPE} ${CHECK} --warmup>> ./${FILE}
 	done
 }
 runtest()
@@ -63,19 +66,19 @@ TEST=1
 
 CHECK="--check"
 ITER=3
-FILE=baseline_fp32_${GPU}_check.log
+FILE=domain_fp32_${GPU}_check.log
 FLOATTYPE="--fp32"
 y=${list_used[0]}
-rm FILE
+rm ${FILE}
 for x in ${list_used[@]};
 do
 	runtest	
 done 
 
-FILE=baseline_fp64_${GPU}_check.log
+FILE=domain_fp64_${GPU}_check.log
 FLOATTYPE="--fp64"
 y=${list_used[0]}
-rm FILE
+rm ${FILE}
 for x in ${list_used[@]};
 do
 	runtest	
@@ -87,20 +90,20 @@ ITER=1000
 TEST=2
 
 
-FILE=baseline_fp32_${GPU}.log
+FILE=domain_fp32_${GPU}.log
 FLOATTYPE="--fp32"
 y=${list_used[0]}
-rm FILE
+rm ${FILE}
 for x in ${list_used[@]};
 do
 	runtest	
 done 
 
 
-FILE=baseline_fp64_${GPU}.log
+FILE=domain_fp64_${GPU}.log
 FLOATTYPE="--fp64"
 y=${list_used[0]}
-rm FILE
+rm ${FILE}
 for x in ${list_used[@]};
 do
 	runtest	
--- stencil/2dstencil/iteratefiles.sh
+++ stencil/2dstencil/iteratefiles.sh
@@ -7,9 +7,19 @@ fi
 # echo "haha wrong"
 
 for file in js2d5pt js2d9pt js2d13pt js2d17pt js2d21pt js2d25pt jb2d9pt jb2d25pt
+# for file in  js2d5pt 
 do
 	cd ./${file}/
 	echo "in" ${file}
-	bash ../iteratedomain_v100.sh
+	# bash ../iteratedomain_v100.sh
+	# bash ../iteratemindomain.sh
+	# cp domain.sh domain.sh.back
+	# python ../ProcessDomaindata.py
+	bash ../iteratebaseline_cm.sh
+	echo "../iteratebaseline_cm.sh"
+	bash ../iterategen.sh
+	echo "../iterategen.sh"
+	bash ../iterategenwr.sh
+	echo "../iterategenwr.sh"
 	cd ../
 done
