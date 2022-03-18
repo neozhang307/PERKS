904d916c800ceb2d7a64750b7b832ca82f1ce4d8
Lingqi <zlqi@qq.com>
2022-03-18 20:11:14 +0900

    make directory consist with paper

--- stencil/2dstencil/makeconsist.sh
+++ stencil/2dstencil/makeconsist.sh
@@ -1,11 +1,11 @@
-cp ./js2d5pt/CMakeLists.txt ./js2d9pt
-cp ./js2d5pt/CMakeLists.txt ./js2d13pt
-cp ./js2d5pt/CMakeLists.txt ./js2d17pt
-cp ./js2d5pt/CMakeLists.txt ./js2d21pt
-cp ./js2d5pt/CMakeLists.txt ./js2d25pt
+cp ./2d5pt/CMakeLists.txt ./2ds9pt
+cp ./2d5pt/CMakeLists.txt ./2d13pt
+cp ./2d5pt/CMakeLists.txt ./2d17pt
+cp ./2d5pt/CMakeLists.txt ./2d21pt
+cp ./2d5pt/CMakeLists.txt ./2ds25pt
 
-cp ./js2d5pt/CMakeLists.txt ./jb2d9pt
-cp ./js2d5pt/CMakeLists.txt ./jb2d25pt
+cp ./2d5pt/CMakeLists.txt ./2d9pt
+cp ./2d5pt/CMakeLists.txt ./2d25pt
 
 
 # cp ./js2d5pt/genconfig.cuh ./js2d9pt
